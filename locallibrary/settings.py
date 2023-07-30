4d98776900e8edf8b9791e6a402031e093987397
Wujun <wujun.ye@hotmail.com>
2023-07-30 19:26:06 +0800

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS with site URL 19:26

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -33,7 +33,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ## For example, for a site URL at 'web-production-3640.up.railway.app'
 ## (replace the string below with your own site URL):
-ALLOWED_HOSTS = ['web-production-3640.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['web-production-9dbf.up.railway.app', '127.0.0.1']
 
 # During development, you can instead set just the base URL
 # (you might decide to change the site a few times).
